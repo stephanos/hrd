f3237b28453d2418dab06d3fe02c3c1d90118576
stephanos <github@stephanbehnke.de>
2014-03-02 17:30:00 +0100

    updated Travis setup script

---
+++ install-sdk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ARCH="386"
+VERSION="1.8.9"
+
+if [[ `uname -a` == *x86_64* ]]
+then
+    ARCH="amd64"
+fi
+
+file=go_appengine_sdk_linux_$ARCH-$VERSION.zip
+echo "downloading '$file'"
+
+wget https://googleappengine.googlecode.com/files/$file -nv
+unzip -q $file -d .
