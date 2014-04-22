9a2f6173a5d32f890dde77d9c835ed2a012ee9df
stephanos <github@stephanbehnke.de>
2014-04-22 09:16:21 +0200

    make install-sdk.sh work with old SDK as well

--- install-sdk.sh
+++ install-sdk.sh
@@ -2,14 +2,16 @@
 
 ARCH="386"
 VERSION="1.9.2"
+SVERSION=$VERSION | sed 's//\./g'
 
 if [[ `uname -a` == *x86_64* ]]
 then
     ARCH="amd64"
 fi
 
-file=go_appengine_sdk_linux_$ARCH-$VERSION.zip
-echo "downloading '$file'"
+FILE=go_appengine_sdk_linux_$ARCH-$VERSION.zip
+echo "downloading '$FILE'"
 
-wget https://commondatastorage.googleapis.com/appengine-sdks/featured/$file -nv
-unzip -q $file -d .
+wget https://commondatastorage.googleapis.com/appengine-sdks/featured/$FILE -nv
+wget https://console.developers.google.com/m/cloudstorage/b/appengine-sdks/o/deprecated/$SVERSION/$FILE -nv
+unzip -q $FILE -d .
