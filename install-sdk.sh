7ab5f5006e92a28d025a6ac50ef4e18dc0728e60
stephanos <github@stephanbehnke.de>
2014-10-25 23:47:12 +0200

    rename variable

--- install-sdk.sh
+++ install-sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION_URL="https://appengine.google.com/api/updatecheck"
-VERSION=$(echo $(curl ${UPDATE_URL}) | sed -E 's/release: \"(.+)\"(.*)/\1/g')
+VERSION=$(echo $(curl ${VERSION_URL}) | sed -E 's/release: \"(.+)\"(.*)/\1/g')
 
 ARCH="386"
 if [[ `uname -a` == *x86_64* ]]
