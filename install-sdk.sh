4fbaa9825cc71b02b3e02c0734404aff44c4c87a
stephanos <github@stephanbehnke.de>
2014-10-25 13:01:45 +0200

    automatically download current SDK version

--- install-sdk.sh
+++ install-sdk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ARCH="386"
-VERSION="1.9.7"
+UPDATE_URL="https://appengine.google.com/api/updatecheck"
+VERSION=$(echo $(curl ${UPDATE_URL}) | sed -E 's/release: \"(.+)\"(.*)/\1/g')
 
+ARCH="386"
 if [[ `uname -a` == *x86_64* ]]
 then
     ARCH="amd64"
