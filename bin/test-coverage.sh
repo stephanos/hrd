9eb6aba89d707456fc649e3530afc4f373d38eec
stephanos <github@stephanbehnke.de>
2014-11-09 15:15:10 +0100

    fix test coverage reporting

---
+++ bin/test-coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "mode: count" > profile.cov
+
+# note: standard go tooling behavior is to ignore dirs with leading underscors
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path '*/_*' -type d);
+do
+if ls $dir/*.go &> /dev/null; then
+    goapp test -covermode=count -coverprofile=$dir/profile.tmp $dir
+    if [ -f $dir/profile.tmp ]
+    then
+        cat $dir/profile.tmp | tail -n +2 >> profile.cov
+        rm $dir/profile.tmp
+    fi
+fi
+done
+
+goapp tool cover -func profile.cov
