9fb353f2391726f717427888869a00791590bd21
stephanos <github@stephanbehnke.de>
2014-11-09 15:37:39 +0100

    add more tests for codec

--- internal/codec_test.go
+++ internal/codec_test.go
@@ -1,92 +1,63 @@
 package internal
 
 import (
-	_ "github.com/101loops/bdd"
+	"time"
+	. "github.com/101loops/bdd"
+	"github.com/101loops/structor"
 )
 
-//var _ = Describe("Codec", func() {
-//
-//	It("return simple codec", func() {
-//		validateSimpleCode()
-//		validateSimpleCode() // ... coming from cache
-//	})
-//
-//	It("return complex codec", func() {
-//		code, err := getCodec(&ComplexModel{})
-//
-//		Check(err, IsNil)
-//		Check(code, NotNil)
-//		Check(code.complete, IsTrue)
-//
-//		Check(code.byIndex, Equals, map[int]tagCodec{
-//			0: tagCodec{name: "tag", tags: []string{}},
-//			//1: tagCodec{name: "pair", tags: []string{}},
-//			1: tagCodec{name: "tags", tags: []string{}},
-//			//3: tagCodec{name: "pairs", tags: []string{}},
-//		})
-//
-//		Check(code.byName, HasLen, 4)
-//		Check(code.byName, HasKeys, "tag.key", "tag.val", "tags.key", "tags.val")
-//		//"pair.key", "pair.val", "pairs.key", "pairs.val"
-//
-//		Check(code.byName["tag.key"].subcodec, NotNil)
-//		Check(*code.byName["tag.key"].subcodec, Equals, pairCodec)
-//		Check(code.byName["tag.val"].subcodec, NotNil)
-//		Check(*code.byName["tag.val"].subcodec, Equals, pairCodec)
-//
-//		//Check(code.byName["pair.key"].subcodec, IsNil)
-//		//Check(*code.byName["pair.key"].subcodec, Equals,  pairCodec)
-//		//Check(code.byName["pair.val"].subcodec, IsNil)
-//		//Check(*code.byName["pair.val"].subcodec, Equals,  pairCodec)
-//
-//		Check(code.byName["tags.key"].subcodec, NotNil)
-//		Check(*code.byName["tags.key"].subcodec, Equals, pairCodec)
-//		Check(code.byName["tags.val"].subcodec, NotNil)
-//		Check(*code.byName["tags.val"].subcodec, Equals, pairCodec)
-//
-//		//Check(code.byName["pairs.key"].subcodec, IsNil)
-//		//Check(*code.byName["pairs.key"].subcodec, Equals,  pairCodec)
-//		//Check(code.byName["pairs.val"].subcodec, IsNil)
-//		//Check(*code.byName["pairs.val"].subcodec, Equals,  pairCodec)
-//
-//		Check(code.hasSlice, IsTrue)
-//	})
-//})
-//
-//func validateSimpleCode() {
-//	code, err := getCodec(&SimpleModel{})
-//
-//	Check(err, IsNil)
-//	Check(code, NotNil)
-//	Check(*code, Equals, hrdCodec{
-//		byIndex: map[int]*{
-//			1: tagCodec{name: "num", tags: []string{}},
-//			3: tagCodec{name: "dat", tags: []string{"index"}},
-//			4: tagCodec{name: "html", tags: []string{"index", "omitempty"}},
-//			5: tagCodec{name: "timing", tags: []string{"index", "omitempty"}},
-//		},
-//		byName: map[string]fieldCodec{
-//			"num":    fieldCodec{index: 1, subcodec: nil},
-//			"dat":    fieldCodec{index: 3, subcodec: nil},
-//			"html":   fieldCodec{index: 4, subcodec: nil},
-//			"timing": fieldCodec{index: 5, subcodec: nil},
-//		},
-//		hasSlice: false,
-//		complete: true,
-//	})
-//}
-//
-//var (
-//	pairCodec = hrdCodec{
-//		byIndex: map[int]tagCodec{
-//			0: tagCodec{name: "key", tags: []string{"index", "omitempty"}},
-//			1: tagCodec{name: "val", tags: []string{}},
-//		},
-//		byName: map[string]fieldCodec{
-//			"key": fieldCodec{index: 0, subcodec: nil},
-//			"val": fieldCodec{index: 1, subcodec: nil},
-//		},
-//		hasSlice: false,
-//		complete: true,
-//	}
-//)
+var _ = Describe("Codec", func() {
+
+	It("evaluate simple codec", func() {
+		type SimpleModel struct {
+			id     int64
+			Ignore string    `datastore:"-"`
+			Num    int64     `datastore:"num"`
+			Data   []byte    `datastore:",index"`
+			Text   string    `datastore:"html,index"`
+			Time   time.Time `datastore:"timing,index,omitempty"`
+		}
+
+		entity := &SimpleModel{}
+
+		err := CodecSet.Add(entity)
+		Check(err, IsNil)
+
+		var code *structor.Codec
+		code, err = GetCodec(entity)
+		Check(err, IsNil)
+		Check(code, NotNil)
+		Check(code.Complete(), IsTrue)
+
+		fieldNames := code.FieldNames()
+		Check(fieldNames, HasLen, 4)
+		Check(fieldNames, Equals, []string{"Num", "Data", "Text", "Time"})
+
+		fields := code.Fields()
+		Check(fields, HasLen, 4)
+	})
+
+	It("evaluate complex codec", func() {
+		type Pair struct {
+			Key string `datastore:"key,index,omitempty"`
+			Val string
+		}
+
+		type ComplexModel struct {
+			Pair Pair `datastore:"tag"`
+			//PairPtr  *Pair   `datastore:"pair"`
+			Pairs []Pair `datastore:"tags"`
+			//PairPtrs []*Pair `datastore:"pairs"`
+		}
+
+		entity := &ComplexModel{}
+
+		err := CodecSet.Add(entity)
+		Check(err, IsNil)
+
+		var code *structor.Codec
+		code, err = GetCodec(entity)
+		Check(err, IsNil)
+		Check(code, NotNil)
+	})
+})
