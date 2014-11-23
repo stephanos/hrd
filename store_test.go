46d6872b58bb9a595fbf451345fe9cbf10ea5f60
stephanos <github@stephanbehnke.de>
2014-11-23 12:28:47 +0100

    increase test coverage

--- store_test.go
+++ store_test.go
@@ -5,8 +5,11 @@ import . "github.com/101loops/bdd"
 var _ = Describe("Store", func() {
 
 	It("initializes and is configurable", func() {
-		Check(myStore.opts, NotNil)
+		Check(myStore.opts.useGlobalCache, IsTrue)
 		Check(myStore.CreatedAt(), Not(IsZero))
+
+		myStore.Opts(NoGlobalCache)
+		Check(myStore.opts.useGlobalCache, IsFalse)
 	})
 
 	It("creates a kind", func() {
@@ -15,5 +18,21 @@ var _ = Describe("Store", func() {
 		Check(newKind.opts, NotNil)
 		Check(newKind.store, Equals, myStore)
 		Check(newKind.Name(), Equals, "new-kind")
+	})
+
+	It("registers a new entity", func() {
+		type MyModel1 struct{}
+		err := myStore.RegisterEntity(&MyModel1{})
+		Check(err, IsNil)
+
+		err = myStore.RegisterEntity("invalid-entity")
+		Check(err, NotNil)
+
+		type MyModel2 struct{}
+		myStore.RegisterEntityMust(&MyModel2{})
+
+		Check(func() {
+			myStore.RegisterEntityMust("invalid-entity")
+		}, Panics)
 	})
 })
