d3428931c7d018771a8b5769240abd24653da5fc
stephanos <github@stephanbehnke.de>
2014-11-23 10:32:38 +0100

    increase test coverage

--- kind_test.go
+++ kind_test.go
@@ -4,49 +4,49 @@ import . "github.com/101loops/bdd"
 
 var _ = Describe("Kind", func() {
 
-	//	It("creates numeric key", func() {
-	//		key := myKind.NewNumKey(42)
-	//
-	//		Check(key.IntID(), EqualsNum, 42)
-	//		Check(key.Parent(), IsNil)
-	//	})
-	//
-	//	It("creates numeric keys", func() {
-	//		keys := myKind.NewNumKeys(1, 2)
-	//
-	//		Check(keys, HasLen, 2)
-	//		Check(keys[0].IntID(), EqualsNum, 1)
-	//		Check(keys[1].IntID(), EqualsNum, 2)
-	//	})
-	//
-	//	It("creates numeric key with parent", func() {
-	//		key := myKind.NewNumKey(42, myKind.NewNumKey(66))
-	//
-	//		Check(key.IntID(), EqualsNum, 42)
-	//		Check(key.Parent(), NotNil)
-	//		Check(key.Parent().IntID(), EqualsNum, 66)
-	//	})
-	//
-	//	It("creates text key", func() {
-	//		key := myKind.NewTextKey("abc")
-	//
-	//		Check(key.StringID(), Equals, "abc")
-	//		Check(key.Parent(), IsNil)
-	//	})
-	//
-	//	It("creates text keys", func() {
-	//		keys := myKind.NewTextKeys("abc", "xyz")
-	//
-	//		Check(keys, HasLen, 2)
-	//		Check(keys[0].StringID(), Equals, "abc")
-	//		Check(keys[1].StringID(), Equals, "xyz")
-	//	})
-	//
-	//	It("creates text key with parent", func() {
-	//		key := myKind.NewTextKey("abc", myKind.NewTextKey("xyz"))
-	//
-	//		Check(key.StringID(), Equals, "abc")
-	//		Check(key.Parent(), NotNil)
-	//		Check(key.Parent().StringID(), Equals, "xyz")
-	//	})
+	It("creates numeric key", func() {
+		key := myKind.NewNumKey(42)
+
+		Check(key.IntID(), EqualsNum, 42)
+		Check(key.Parent(), IsNil)
+	})
+
+	It("creates numeric keys", func() {
+		keys := myKind.NewNumKeys(1, 2)
+
+		Check(keys, HasLen, 2)
+		Check(keys[0].IntID(), EqualsNum, 1)
+		Check(keys[1].IntID(), EqualsNum, 2)
+	})
+
+	It("creates numeric key with parent", func() {
+		key := myKind.NewNumKey(42, myKind.NewNumKey(66))
+
+		Check(key.IntID(), EqualsNum, 42)
+		Check(key.Parent(), NotNil)
+		Check(key.Parent().IntID(), EqualsNum, 66)
+	})
+
+	It("creates text key", func() {
+		key := myKind.NewTextKey("abc")
+
+		Check(key.StringID(), Equals, "abc")
+		Check(key.Parent(), IsNil)
+	})
+
+	It("creates text keys", func() {
+		keys := myKind.NewTextKeys("abc", "xyz")
+
+		Check(keys, HasLen, 2)
+		Check(keys[0].StringID(), Equals, "abc")
+		Check(keys[1].StringID(), Equals, "xyz")
+	})
+
+	It("creates text key with parent", func() {
+		key := myKind.NewTextKey("abc", myKind.NewTextKey("xyz"))
+
+		Check(key.StringID(), Equals, "abc")
+		Check(key.Parent(), NotNil)
+		Check(key.Parent().StringID(), Equals, "xyz")
+	})
 })
