fe289f4bc68922092b9c1e177938eb657e1264f1
stephanos <github@stephanbehnke.de>
2014-11-28 20:37:27 +0100

    add tests for setKey()

--- internal/trafo/doc_test.go
+++ internal/trafo/doc_test.go
@@ -3,6 +3,10 @@ package trafo
 import (
 	"reflect"
 	. "github.com/101loops/bdd"
+	"github.com/101loops/hrd/internal/fixture"
+	"github.com/101loops/hrd/internal/types"
+
+	ds "appengine/datastore"
 )
 
 var _ = Describe("Doc", func() {
@@ -28,7 +32,7 @@ var _ = Describe("Doc", func() {
 			Check(doc, NotNil)
 		})
 
-		It("unkown struct", func() {
+		It("unknown struct", func() {
 			doc, err := newDocFromInst(UnknownModel{})
 			Check(doc, IsNil)
 			Check(err, NotNil).And(Contains, "no registered codec found for type 'trafo.UnknownModel'")
@@ -53,6 +57,57 @@ var _ = Describe("Doc", func() {
 			doc, err := newDocFromType(reflect.TypeOf(&UnknownModel{}))
 			Check(doc, IsNil)
 			Check(err, NotNil).And(Contains, "no registered codec found for type 'trafo.UnknownModel'")
+		})
+	})
+
+	Context("set key", func() {
+
+		It("with numeric id", func() {
+			entity := fixture.EntityWithNumID{}
+			CodecSet.AddMust(entity)
+
+			doc, _ := newDocFromInst(&entity)
+			doc.setKey(types.NewKey(ds.NewKey(ctx, "my-kind", "", 42, nil)))
+
+			Check(entity.ID(), EqualsNum, 42)
+		})
+
+		It("with text id", func() {
+			entity := fixture.EntityWithTextID{}
+			CodecSet.AddMust(entity)
+
+			doc, _ := newDocFromInst(&entity)
+			doc.setKey(types.NewKey(ds.NewKey(ctx, "my-kind", "abc", 0, nil)))
+
+			Check(entity.ID(), Equals, "abc")
+		})
+
+		It("with numeric parent id", func() {
+			entity := fixture.EntityWithParentNumID{}
+			CodecSet.AddMust(entity)
+
+			doc, _ := newDocFromInst(&entity)
+			doc.setKey(types.NewKey(ds.NewKey(ctx, "my-kind", "", 1,
+				ds.NewKey(ctx, "my-kind", "", 2, nil))))
+
+			Check(entity.ID(), EqualsNum, 1)
+			parentKind, parentID := entity.Parent()
+			Check(parentKind, Equals, "my-kind")
+			Check(parentID, EqualsNum, 2)
+		})
+
+		It("with text parent id", func() {
+			entity := fixture.EntityWithParentTextID{}
+			CodecSet.AddMust(entity)
+
+			doc, _ := newDocFromInst(&entity)
+			doc.setKey(types.NewKey(ds.NewKey(ctx, "my-kind", "abc", 0,
+				ds.NewKey(ctx, "my-kind", "xyz", 0, nil))))
+
+			Check(entity.ID(), Equals, "abc")
+			parentKind, parentID := entity.Parent()
+			Check(parentKind, Equals, "my-kind")
+			Check(parentID, Equals, "xyz")
 		})
 	})
 })
