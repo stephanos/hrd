601215501167c3b89085f177dcec6f6e1583d4ea
stephanos <github@stephanbehnke.de>
2014-11-28 18:13:14 +0100

    add more tests

--- internal/trafo/doc_test.go
+++ internal/trafo/doc_test.go
@@ -1,17 +1,58 @@
 package trafo
 
-import . "github.com/101loops/bdd"
+import (
+	"reflect"
+	. "github.com/101loops/bdd"
+)
 
 var _ = Describe("Doc", func() {
 
-	It("is created from instance", func() {
-		doc, err := newDocFromInst(&SimpleModel{
-			Num:  42,
-			Text: "html",
-			Data: []byte("byte"),
+	type MyModel struct{}
+	type UnknownModel struct{}
+
+	BeforeSuite(func() {
+		CodecSet.AddMust(MyModel{})
+	})
+
+	Context("create from instance", func() {
+
+		It("known struct", func() {
+			doc, err := newDocFromInst(MyModel{})
+			Check(err, IsNil)
+			Check(doc, NotNil)
 		})
 
-		Check(err, IsNil)
-		Check(doc, NotNil)
+		It("pointer to known struct", func() {
+			doc, err := newDocFromInst(&MyModel{})
+			Check(err, IsNil)
+			Check(doc, NotNil)
+		})
+
+		It("unkown struct", func() {
+			doc, err := newDocFromInst(UnknownModel{})
+			Check(doc, IsNil)
+			Check(err, NotNil).And(Contains, "no registered codec found for type 'trafo.UnknownModel'")
+		})
+
+		It("pointer to unknown struct", func() {
+			doc, err := newDocFromInst(&UnknownModel{})
+			Check(doc, IsNil)
+			Check(err, NotNil).And(Contains, "no registered codec found for type 'trafo.UnknownModel'")
+		})
+	})
+
+	Context("create from type", func() {
+
+		It("pointer to known struct", func() {
+			doc, err := newDocFromType(reflect.TypeOf(&MyModel{}))
+			Check(err, IsNil)
+			Check(doc, NotNil)
+		})
+
+		It("pointer to unknown struct", func() {
+			doc, err := newDocFromType(reflect.TypeOf(&UnknownModel{}))
+			Check(doc, IsNil)
+			Check(err, NotNil).And(Contains, "no registered codec found for type 'trafo.UnknownModel'")
+		})
 	})
 })
