f657738246c41975f16502f816e3610edd6eaf6c
stephanos <github@stephanbehnke.de>
2014-11-29 17:04:49 +0100

    add more tests

--- internal/trafo/load_test.go
+++ internal/trafo/load_test.go
@@ -1,37 +1,102 @@
 package trafo
 
 import (
+	"fmt"
 	. "github.com/101loops/bdd"
 
 	ds "appengine/datastore"
 )
 
+type loadEntity struct {
+	A string
+	B int
+
+	beforeFunc func() error
+	afterFunc  func() error
+}
+
+func (l *loadEntity) BeforeLoad() error {
+	if l.beforeFunc != nil {
+		return l.beforeFunc()
+	}
+	return nil
+}
+
+func (l *loadEntity) AfterLoad() error {
+	if l.afterFunc != nil {
+		return l.afterFunc()
+	}
+	return nil
+}
+
 var _ = Describe("Doc: Load", func() {
 
-	It("should load an entity from channel properties", func() {
-		type MyModel struct {
-			A string
-			B int
-		}
-		CodecSet.AddMust(MyModel{})
+	var entity loadEntity
 
-		doc, err := newDocFromInst(&MyModel{})
+	BeforeEach(func() {
+		entity = loadEntity{}
+		CodecSet.AddMust(entity)
+	})
+
+	It("should load an entity from channel properties", func() {
+		doc, err := newDocFromInst(&entity)
 		Check(err, IsNil)
 		Check(doc, NotNil)
 
-		c := make(chan ds.Property)
-		go func() {
-			c <- ds.Property{Name: "A", Value: "abc"}
-			c <- ds.Property{Name: "B", Value: int64(1)}
-			close(c)
-		}()
-
+		c := newPropertyChannel()
 		err = doc.Load(c)
 		Check(err, IsNil)
 		Check(c, IsClosed)
 
-		res := (doc.get()).(*MyModel)
+		res := (doc.get()).(*loadEntity)
+		Check(res.A, Equals, "abc")
+		Check(res.B, EqualsNum, 1)
+	})
+
+	It("should return an error when BeforeLoad errs", func() {
+		entity.beforeFunc = func() error {
+			return fmt.Errorf("an error")
+		}
+
+		doc, err := newDocFromInst(&entity)
+		Check(err, IsNil)
+		Check(doc, NotNil)
+
+		c := newPropertyChannel()
+		err = doc.Load(c)
+		Check(err, ErrorContains, "an error")
+		Check(c, IsClosed)
+
+		res := (doc.get()).(*loadEntity)
+		Check(res.A, Equals, "")
+	})
+
+	It("should return an error when AfterLoad errs", func() {
+		entity.afterFunc = func() error {
+			return fmt.Errorf("an error")
+		}
+
+		doc, err := newDocFromInst(&entity)
+		Check(err, IsNil)
+		Check(doc, NotNil)
+
+		c := newPropertyChannel()
+		err = doc.Load(c)
+		Check(err, ErrorContains, "an error")
+		Check(c, IsClosed)
+
+		res := (doc.get()).(*loadEntity)
 		Check(res.A, Equals, "abc")
 		Check(res.B, EqualsNum, 1)
 	})
 })
+
+func newPropertyChannel() chan ds.Property {
+	c := make(chan ds.Property)
+	go func() {
+		c <- ds.Property{Name: "A", Value: "abc"}
+		c <- ds.Property{Name: "B", Value: int64(1)}
+		close(c)
+	}()
+	return c
+}
