c84e630c88e45425a5e7e0b40cc990806de32f35
stephanos <github@stephanbehnke.de>
2014-12-12 15:45:10 +0100

    test error case as well

--- internal/types/iterator_test.go
+++ internal/types/iterator_test.go
@@ -22,17 +22,28 @@ var _ = Describe("Iterator", func() {
 		Check(it.inner, NotNil)
 	})
 
-	It("should return a cursor", func() {
-		cursor, err := NewIterator(ctx, qry).Cursor()
-
-		Check(err, IsNil)
-		Check(cursor, IsZero)
-	})
-
 	It("should return next entity", func() {
 		_, err := NewIterator(ctx, qry).Next(func(_ ae.Context) ds.PropertyLoadSaver {
 			return nil
 		})
 		Check(err, Equals, ds.Done)
+	})
+
+	Context("cursor", func() {
+
+		It("should return a cursor", func() {
+			cursor, err := NewIterator(ctx, qry).Cursor()
+
+			Check(err, IsNil)
+			Check(cursor, IsZero)
+		})
+
+		It("should return an error for invalid query", func() {
+			qry.Filter = []Filter{Filter{"age !=", 18}}
+			cursor, err := NewIterator(ctx, qry).Cursor()
+
+			Check(cursor, IsZero)
+			Check(err, HasOccurred).And(Contains, "invalid operator")
+		})
 	})
 })
